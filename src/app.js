9acaee215898b3ebcf168f98ed60ade48ba99c66
Koteterov <koteterov@yahoo.com>
2022-08-11 12:13:21 +0300

    html structure and routing added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
+import { page } from "./lib.js";
+import { render } from "./lib.js";
+import { aboutPage } from "./views/about.js";
+import { homePage } from "./views/home.js";
+import { productsPage } from "./views/products.js";
+
+
+const bodyEl = document.querySelector('body')
+
+
+page(decorateContext);
+
+page("/index.html", "/");
+page("/", homePage);
+page("/products", productsPage)
+page("/about", aboutPage)
+
+
+page.start();
+
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, bodyEl);
+
+    next()
+}  
+
+
 console.log(11);
---
+++ src/views/home.js
@@ -0,0 +1,143 @@
+import { html, nothing } from "../lib.js";
+
+const homeTemplate = () => html`
+    <!-- navbar -->
+    <nav class="navbar">
+      <div class="nav-center">
+        <!-- links -->
+        <div>
+          <button class="toggle-nav">
+            <i class="fas fa-bars"></i>
+          </button>
+          <ul class="nav-links">
+            <li>
+              <a href="/" class="nav-link">
+                home
+              </a>
+            </li>
+            <li>
+              <a href="/products" class="nav-link">
+                products
+              </a>
+            </li>
+            <li>
+              <a href="/about" class="nav-link">
+                about
+              </a>
+            </li>
+          </ul>
+        </div>
+        <!-- logo -->
+        <span class="logo-text nav-logo">'Wine is Fine'</span>
+        <!-- cart icon -->
+        <div class="toggle-container">
+          <button class="toggle-cart">
+            <i class="fas fa-shopping-cart"></i>
+          </button>
+          <span class="cart-item-count">1</span>
+        </div>
+      </div>
+    </nav>
+    <!-- hero -->
+    <section class="hero">
+      <div class="hero-container">
+        <h1 class="text-slanted">
+          chose, taste, come back
+        </h1>
+        <h3>Make your choice - we are here to help</h3>
+        <a href="/products" class="hero-btn">
+          show now
+        </a>
+      </div>
+    </section>
+    <!-- sidebar -->
+    <div class="sidebar-overlay">
+      <aside class="sidebar">
+        <!-- close -->
+        <button class="sidebar-close">
+          <i class="fas fa-times"></i>
+        </button>
+        <!-- links -->
+        <ul class="sidebar-links">
+          <li>
+            <a href="/" class="sidebar-link">
+              <i class="fas fa-home fa-fw"></i>
+              home
+            </a>
+          </li>
+          <li>
+            <a href="/products" class="sidebar-link">
+              <i class="fas fa-couch fa-fw"></i>
+              products
+            </a>
+          </li>
+          <li>
+            <a href="/about" class="sidebar-link">
+              <i class="fas fa-book fa-fw"></i>
+              about
+            </a>
+          </li>
+        </ul>
+      </aside>
+    </div>
+    <!-- cart -->
+    <div class="cart-overlay">
+      <aside class="cart">
+        <button class="cart-close">
+          <i class="fas fa-times"></i>
+        </button>
+        <header>
+          <h3 class="text-slanted">your bag</h3>
+        </header>
+        <!-- cart items -->
+        <div class="cart-items"></div>
+        <!-- footer -->
+        <footer>
+          <h3 class="cart-total text-slanted">
+            total : $12.99
+          </h3>
+          <button class="cart-checkout btn">checkout</button>
+        </footer>
+      </aside>
+    </div>
+    <!-- featured products -->
+    <section class="section featured">
+      <div class="title">
+        <h2><span>/</span>Wines of the Week</h2>
+      </div>
+      <div class="featured-center section-center">
+        <h2 class="section-loading">
+          loading...
+        </h2>
+        <!-- single product -->
+        <!-- <article class="product">
+          <div class="product-container">
+            <img src="./images/main-bcg.jpeg" class="product-img img" alt="" />
+           
+            <div class="product-icons">
+              <a href="product.html?id=1" class="product-icon">
+                <i class="fas fa-search"></i>
+              </a>
+              <button class="product-cart-btn product-icon" data-id="1">
+                <i class="fas fa-shopping-cart"></i>
+              </button>
+            </div>
+          </div>
+          <footer>
+            <p class="product-name">name</p>
+            <h4 class="product-price">$9.99</h4>
+          </footer>
+        </article> -->
+        <!-- end of single product -->
+      </div>
+      <a href="/products" class="btn">
+        all products
+      </a>
+    </section>
+
+`
+
+export async function homePage(ctx) {
+    ctx.render(homeTemplate());
+  }
+  
