f687a26beed07885f9d937d136794c5635593ccb
Koteterov <koteterov@yahoo.com>
2022-08-26 13:51:13 +0300

    some work on wine display styles

--- src/views/notFound.js
+++ src/views/notFound.js
@@ -1,6 +1,6 @@
 import { html } from "../lib.js";
 
-const notFoundTemplate = () => html` <h2>Not found</h2>`;
+const notFoundTemplate = () => html`<h2>404 Not Found</h2>`;
 
 export function notFoundPage(ctx) {
   ctx.render(notFoundTemplate());
