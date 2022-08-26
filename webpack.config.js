9ecebcb0de4d3ebece1d5fe05c31605c556b8bc0
Koteterov <koteterov@yahoo.com>
2022-08-26 16:59:38 +0300

    webpack added to project

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin') 
+
+module.exports = {
+    mode: 'production',
+    devtool: false,
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'template.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset'
+            }
+        ]
+    },
+    experiments: {
+        topLevelAwait: true
+    },
+};
